e61a9725eced4232080c41cea83e060047c53cd4
S M Junayed Shanto <smjunayedshanto@gmail.com>
2023-03-18 01:46:20 +0600

    added department & student service as eureka client to eureka server

--- service-registry/src/main/java/com/shanto/microservice/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/shanto/microservice/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,19 @@ package com.shanto.microservice.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+//@ConditionalOnProperty(
+//		value = {"eureka.client.register-with-eureka",
+//				"eureka.client.fetch-registry"},
+//		matchIfMissing = false
+//)
+
+//"spring.cloud.service-registry.auto-registration.enabled",
+//		"spring.cloud.discovery.enabled",
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
